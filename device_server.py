e66e1ce62854bd13f33a6a5e4f3a8e84636fdbc0
zwh2119 <1548767361@qq.com>
2022-05-30 02:45:45 +0800

    modify server

--- device_server.py
+++ device_server.py
@@ -85,7 +85,7 @@ def get_model():
     return download_file
 
 
-@device_server.put('/model/<algo>')
+@device_server.put('/model/<al>')
 def put_model(algo):
 
     if not device_solution.check_algo(algo):
